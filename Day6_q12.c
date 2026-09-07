6ca48eb505f3621d9250a2b8f834a07eb4e2a649
vermaujjwal0999-lgtm <vermaujjwal0999@gmail.com>
2026-09-07 13:07:31 +0530

    Day6 q11 and q12

---
+++ Day6_q12.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+    int num;
+
+    printf("Enter an integer: ");
+    scanf("%d", &num);
+
+    if (num >= 0)
+    {
+        if (num == 0)
+        {
+            printf("Zero\n");
+        }
+        else
+        {
+            printf("Positive\n");
+        }
+    }
+    else
+    {
+        printf("Negative\n");
+    }
+    return 0;
+}
