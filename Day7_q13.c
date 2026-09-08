fcd62e74535a36d654a4cc342b7c1a3d305ea52f
vermaujjwal0999-lgtm <vermaujjwal0999@gmail.com>
2026-09-08 22:11:52 +0530

    Day7 q13 and q14

---
+++ Day7_q13.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int year;
+
+    printf("Enter a year: ");
+    scanf("%d", &year);
+
+    if (year % 400 == 0) {
+        printf("Leap Year\n");
+    } else if (year % 100 == 0) {
+        printf("Not a Leap Year\n");
+    } else if (year % 4 == 0) {
+        printf("Leap Year\n");
+    } else {
+        printf("Not a Leap Year\n");
+    }
+
+    return 0;
+}
