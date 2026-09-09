78ea2b21095035593f9706748b8b69fe99fa4f48
ujjwal.verma <vermajjwal0999@gmai.com>
2026-09-09 21:25:40 +0530

    submit

---
+++ Day8_q15.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+ {
+    char ch;
+
+    printf("Enter a character: ");
+    scanf(" %c", &ch);
+
+    if (ch >= 'A' && ch <= 'Z') {
+        printf("Uppercase Alphabet\n");
+    } else if (ch >= 'a' && ch <= 'z') {
+        printf("Lowercase Alphabet\n");
+    } else if (ch >= '0' && ch <= '9') {
+        printf("Digit\n");
+    } else {
+        printf("Special Character\n");
+    }
+
+    return 0;
+}
