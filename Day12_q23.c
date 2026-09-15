72f38fe9bdca788ce5bf1a644e399522538f7fb6
vermaujjwal0999-lgtm <vermaujjwal0999@gmail.com>
2026-09-15 11:23:54 +0530

    Day12 Q23 and q24

---
+++ Day12_q23.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int days;
+    float fine;
+
+    printf("Enter late days: ");
+    scanf("%d", &days);
+
+    if (days <= 5) {
+        fine = days * 2;
+        printf("Fine = Rs. %.2f\n", fine);
+    } else if (days <= 10) {
+        fine = 5 * 2 + (days - 5) * 4;
+        printf("Fine = Rs. %.2f\n", fine);
+    } else if (days <= 30) {
+        fine = 5 * 2 + 5 * 4 + (days - 10) * 6;
+        printf("Fine = Rs. %.2f\n", fine);
+    } else {
+        printf("Membership Cancelled\n");
+    }
+
+    return 0;
+}
