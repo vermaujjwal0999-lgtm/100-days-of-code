f3cb726f9d156774c76d5148b6febd3e28bb3af8
vermaujjwal0999-lgtm <vermaujjwal0999@gmail.com>
2026-09-17 15:46:34 +0530

    Day14 q27 and q28

---
+++ Day14_q28.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    int product = 1;
+
+    printf("Enter n: ");
+    scanf("%d", &n);
+
+    for(i = 2; i <= n; i = i + 2) {
+        product = product * i;
+    }
+
+    printf("Product = %d\n", product);
+
+    return 0;
+}
